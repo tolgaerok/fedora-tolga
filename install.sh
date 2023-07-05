957fc23afb902c33a353b46c8d630c1fe829a4eb
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-05 21:32:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,20 +18,82 @@ SMB_DIR="$builddir/SAMBA"
 WALLPAPERS_DIR="$builddir/WALLPAPERS"
 
 # Update packages list and update system
-apt update
-apt upgrade -y
+sudo dnf autoremove -y
+sudo fwupdmgr get-devices
+sudo fwupdmgr refresh --force
+sudo fwupdmgr get-updates
+sudo fwupdmgr update -y
 
 # Install nala first
-sudo apt install -y nala
+sudo dnf install -y nala
 
 # Install some software
-sudo apt install -y gdebi flatpak firmware-realtek bluez bluez-tools libavcodec-extra vlc samba synaptic cifs-utils gnome-software-plugin-flatpak
-sudo apt install -y acl attr samba winbind libpam-winbind libnss-winbind krb5-config krb5-user dnsutils python3-setproctitle ntp chrony
-sudo apt install -y plocate sntp ntpdate software-properties-common terminator htop neofetch simplescreenrecorder rhythmbox
+## Install mpg123
+echo -e "Install MP3 codec...\n"
+
+sudo dnf install -y mpg123
+
+sleep 2
+clear
+
+echo -e "Installing Tweaks, media codecs, extensions & plugins\n"
+
+sudo dnf groupupdate -y sound-and-video
+sudo dnf install -y libdvdcss libdrm-devel gtk3-devel gcc pkg-config --allowerasing --skip-broken
+sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg --allowerasing --skip-broken
+sudo dnf install -y lame\* --exclude=lame-devel
+sudo dnf group upgrade -y --with-optional Multimedia
+
+sleep 2
+clear
 
 # Set-up flatpak and some flatpak software
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install -y flathub com.sindresorhus.Caprine
+echo -e "Install Flatpak apps...\n"
+
+## Enable Flatpak
+if ! sudo flatpak remote-list | grep -q "flathub"; then
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+fi
+
+## Update Flatpak
+sudo flatpak update
+
+## Install flatpak apps
+packages=(
+    app.drey.Dialect
+    com.anydesk.Anydesk
+    com.bitwarden.desktop
+    com.calibre_ebook.calibre
+    com.getpostman.Postman
+    com.github.unrud.VideoDownloader
+    com.sindresorhus.Caprine
+    com.sublimetext.three
+    com.transmissionbt.Transmission
+    com.wps.Office
+    io.github.aandrew_me.ytdn
+    io.github.ltiber.Pwall
+    io.github.mimbrero.WhatsAppDesktop
+    io.gitlab.o20.word
+    org.audacityteam.Audacity
+    org.gimp.GIMP
+    org.gnome.gitlab.YaLTeR.VideoTrimmer
+    org.gnome.Shotwell
+    org.kde.krita
+    org.kde.kweather
+)
+
+# Install each package if not already installed
+for package in "${packages[@]}"; do
+    if ! sudo flatpak list | grep -q "$package"; then
+        echo "Installing $package..."
+        sudo flatpak install -y flathub "$package"
+    else
+        echo "$package is already installed. Skipping..."
+    fi
+done
+
+## End
+echo -e "\nSoftware install complete..."
 
 # Download teamviewer
 download_url="https://dl.teamviewer.com/download/linux/version_15x/teamviewer_15.43.6_amd64.deb?utm_source=google&utm_medium=cpc&utm_campaign=au%7Cb%7Cpr%7C22%7Cjun%7Ctv-core-brand-only-exact-sn%7Cfree%7Ct0%7C0&utm_content=Exact&utm_term=teamviewer&ref=https%3A%2F%2Fwww.teamviewer.com%2Fen-au%2Fdownload%2Flinux%2F%3Futm_source%3Dgoogle%26utm_medium%3Dcpc%26utm_campaign%3Dau%257Cb%257Cpr%257C22%257Cjun%257Ctv-core-brand-only-exact-sn%257Cfree%257Ct0%257C0%26utm_content%3DExact%26utm_term%3Dteamviewer"
@@ -82,52 +144,20 @@ sudo apt update && sudo apt install -y powershell
 # Start PowerShell
 # pwsh
 
-echo "$USER"
-sudo sh -c 'echo "deb https://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
-deb-src https://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
-deb https://deb.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
-deb-src https://deb.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
-deb https://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
-deb-src https://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
-deb https://deb.debian.org/debian bookworm-backports main contrib non-free non-free-firmware
-deb-src https://deb.debian.org/debian bookworm-backports main contrib non-free non-free-firmware
-" > /etc/apt/sources.list'
+read -r -p "Installing Nvidia..." -t 2 -n 1 -s
 
-sudo apt update && apt list --upgradable && sudo apt upgrade -y
+## Update system
+echo -e "\e[94mSpeeding Up DNF before installing Nvidia and RPM fusions\e[0m\n"
 
-# Check GPU information
-gpu_info=$(lspci | grep -i 'VGA\|3D')
-if [[ -z $gpu_info ]]; then
-    echo "No GPU found."
-    exit 1
-fi
+echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
+echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
 
-# Check if NVIDIA GPU is present
-if [[ $gpu_info =~ "NVIDIA" ]]; then
-    # Check if NVIDIA drivers are already installed
-    if nvidia-smi &>/dev/null; then
-        read -r -p "NVIDIA drivers are already installed" -t 2 -n 1 -s
-        echo "."
-    else
-        # Install NVIDIA drivers
-        sudo apt update
-        sudo apt install nvidia-driver firmware-misc-nonfree -y
-        sudo apt install -y nvidia-driver
-        echo "NVIDIA drivers installed successfully."
-    fi
-
-    # Run NVIDIA settings
-    sudo nvidia-settings
-
-else
-    # Install video acceleration for HD Intel i965
-    sudo apt update
-    sudo apt install -y i965-va-driver libva-drm2 libva-x11-2 vainfo
-    echo "Video acceleration drivers installed successfully."
-fi
+sleep 3
+clear
 
 # Samba configurations files
-read -r -p "Copying samba files? 
+read -r -p "Copying samba files?
 " -t 2 -n 1 -s
 
 # Define the backup folder path
@@ -194,9 +224,9 @@ Continuing..." -t 1 -n 1 -s
 read -r -p "Create and configure custom samba folder
 " -t 2 -n 1 -s
 
-sudo mkdir /home/Deb12
-sudo chgrp samba /home/Deb12
-sudo chmod 770 /home/Deb12
+sudo mkdir /home/fedora
+sudo chgrp samba /home/fedora
+sudo chmod 770 /home/fedora
 
 read -r -p "
 Continuing..." -t 1 -n 1 -s
@@ -352,5 +382,89 @@ sudo fstrim -av
 echo ""
 echo "Operation completed."
 echo ""
+
+## Enable Free and non-free RPM fusions
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y rpmfusion-free-release-tainted --allowerasing --skip-broken
+
+## Update core-system
+sudo dnf groupupdate core -y
+sudo dnf update -y # and reboot if you are not on the latest kernel
+sudo dnf upgrade --refresh
+sudo dnf groupupdate -y core
+sudo dnf install -y dnf-plugins-core
+sudo dnf autoremove -y
+sudo fwupdmgr get-devices
+sudo fwupdmgr refresh --force
+sudo fwupdmgr get-updates
+sudo fwupdmgr update -y
+
+## Install Nvidia
+sudo dnf -y install kmod-nvidia
+sudo dnf install -y akmod-nvidia             # rhel/centos users can use kmod-nvidia instead
+sudo dnf install -y xorg-x11-drv-nvidia-cuda #optional for cuda/nvdec/nvenc support
+sudo dnf install -y xorg-x11-drv-nvidia-cuda-libs
+sudo dnf install -y vdpauinfo libva-vdpau-driver libva-utils
+sudo dnf install -y vulkan
+sudo dnf autoremove -y
+
+clear
+
+read -r -p "Cleaning up grub and uninstall nouveau driver
+" -t 2 -n 1 -s
+
+# Path to the grub configuration file
+grub_file="/etc/default/grub"
+
+## Append ‘blacklist nouveau’ create file if null
+echo "blacklist nouveau" | sudo tee -a /etc/modprobe.d/blacklist.conf >/dev/null
+
+# Comment out the existing GRUB_CMDLINE_LINUX line
+sed -i 's/^GRUB_CMDLINE_LINUX=/#&/' "$grub_file"
+
+# Add the new GRUB_CMDLINE_LINUX line after the commented line
+sed -i '/^#GRUB_CMDLINE_LINUX=/a GRUB_CMDLINE_LINUX="rhgb quiet rd.driver.blacklist=nouveau"' "$grub_file"
+
+## Update grub2 conf --> BIOS and UEFI ##
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
+# Remove xorg-x11-drv-nouveau
+sudo dnf remove xorg-x11-drv-nouveau
+
+## Backup old initramfs nouveau image ##
+#sudo mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img
+
+## Create new initramfs image ##
+#sudo dracut /boot/initramfs-$(uname -r).img $(uname -r)
+
+## Configure Nvidia while we're here
+sudo nvidia-settings
+
+read -r -p "
+..... Complete" -t 1 -n 1 -s
+
+# Display Nvidia version installed
+driver_version=$(modinfo -F version nvidia 2>/dev/null)
+if [ -n "$driver_version" ]; then
+    echo -e "\e[33mNVIDIA driver version: $driver_version\e[0m"
+else
+    echo -e "\e[33mNVIDIA driver not found.\e[0m"
+fi
+
+duration=240 # 4 minutes
+seconds_remaining=$duration
+
+echo -e "\e[1;33mWaiting\e[0m for 4 minutes before reboot..."
+
+while [ $seconds_remaining -gt 0 ]; do
+    echo -ne "\e[1;31mTime remaining: $seconds_remaining seconds\e[0m \r"
+    sleep 1
+    seconds_remaining=$((seconds_remaining - 1))
+done
+
+echo "Time's up! Rebooting now..."
+
+# Reboot the system
+reboot
 
 exit 0

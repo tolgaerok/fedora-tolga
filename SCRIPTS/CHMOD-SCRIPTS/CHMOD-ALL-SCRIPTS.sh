4c1976570262d9cf6bafc9329e04f2822e70ee67
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-06 22:02:05 +0800

    Update CHMOD-ALL-SCRIPTS.sh

--- SCRIPTS/CHMOD-SCRIPTS/CHMOD-ALL-SCRIPTS.sh
+++ SCRIPTS/CHMOD-SCRIPTS/CHMOD-ALL-SCRIPTS.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#  Tolga Erok  6/7/2023
-#  Personal script that retrieves the current working directory, searches for files with the ".sh" extension 
-#  within that directory, and makes those files executable within the (PWD)
+# Tolga Erok 6/7/2023
+# Personal script that retrieves the current working directory, searches for files with the ".sh" extension
+# within that directory, and makes those files executable within the (PWD)
 
 # Get the current working directory
 folder="$(pwd)"
 
 # Find all files ending with the .sh extension inside the folder
-find "$folder" -type f -name "*.sh" -exec bash -c 'echo -e "\e[34mFolder: \e[97m'"$folder"'\e[0m\n\e[33mScript: \e[94m{} \e[0m\n\e[32mNow executable.\n\e[0m"' \;
+find "$folder" -type f -name "*.sh" -exec chmod +ux {} \; -exec bash -c 'echo -e "\e[34mFolder: \e[97m'"$folder"'\e[0m\n\e[33mScript: \e[94m{} \e[0m\n\e[32mNow executable.\e[0m"' \;
